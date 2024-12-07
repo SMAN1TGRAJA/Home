78b55794aa54523a1aaf5aeb1e4366a8e0ed9ff7
SMAN1TGRAJA <lawakanimasi0@gmail.com>
2024-12-07 19:38:03 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-      // Toggle Menu
-const hamburger = document.getElementById('hamburger');
-const menu = document.getElementById('menu');
-
-hamburger.addEventListener('click', () => {
-    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
-});
